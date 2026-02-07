52aa940800eaaff433273f470fb0d5e64fcdfcaf
romainD <romain.deschamps61@sfr.fr>
2026-02-07 23:32:51 +0100

    Update TLD 2.51

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -4,8 +4,8 @@ using System.Reflection;
 
 [assembly: AssemblyTitle("KeroseneLampTweaks")]
 [assembly: AssemblyCopyright("Created by Xpazeman")]
-[assembly: AssemblyVersion("2.4.0")]
-[assembly: AssemblyFileVersion("2.4.0")]
+[assembly: AssemblyVersion("2.4.1")]
+[assembly: AssemblyFileVersion("2.4.1")]
 
-[assembly: MelonInfo(typeof(KeroseneLampTweaks.Main), "KeroseneLampTweaks", "2.4.0", "Romain, Xpazeman")]
+[assembly: MelonInfo(typeof(KeroseneLampTweaks.Main), "KeroseneLampTweaks", "2.4.1", "Romain, Xpazeman")]
 [assembly: MelonGame("Hinterland", "TheLongDark")]
